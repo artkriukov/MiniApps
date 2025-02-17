4fe6aaafe0aa728c017890943cb7eebfc75302c1
Artem Kriukov <art.kriukov@gmail.com>
2025-02-17 10:40:39 +0300

    start project

---
+++ Tipsy/Tipsy/Controllers/CalculateViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CalculateViewController.swift
+//  Tipsy
+//
+//  Created by Artem Kriukov on 17.02.2025.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
