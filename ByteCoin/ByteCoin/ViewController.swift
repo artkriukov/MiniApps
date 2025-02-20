49a87ab516e3b03aecdb8dd28734b180388800a6
Artem <art.kriukov@gmail.com>
2025-02-20 14:42:11 +0300

    initial project

---
+++ ByteCoin/ByteCoin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ByteCoin
+//
+//  Created by Artem Kriukov on 20.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
