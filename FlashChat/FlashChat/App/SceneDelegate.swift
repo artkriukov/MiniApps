ad9598da7f5b8a807d2babecc38cda7f79ef7d3f
Artem <art.kriukov@gmail.com>
2025-02-21 15:45:08 +0300

    add assets

---
+++ FlashChat/FlashChat/App/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  FlashChat
+//
+//  Created by Artem Kriukov on 21.02.2025.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+
+   
+}
+
